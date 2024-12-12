c1d8521a709981371ec1a36cfb66693dff4499cb
SaraRow <hola@sararow.es>
2024-12-12 09:24:42 +0100

    Default ui component classes

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -21,7 +21,7 @@ const buttonVariants = cva(
 				link: 'text-primary underline-offset-4 hover:underline',
 			},
 			size: {
-				default: 'h-12 px-8 py-3',
+				default: 'h-12 px-8 py-3 rounded-lg',
 				sm: 'h-11 rounded-md px-5',
 				lg: 'h-11 rounded-md px-8',
 				icon: 'h-10 w-10',
